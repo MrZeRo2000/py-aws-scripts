77d12386febd817ae8164082fbfb11e77024192b
MrZeRo2000 <MrZeRo2020@outlook.com>
2024-03-22 10:53:04 +0200

    added run script

--- saml2aws.ps1
+++ saml2aws.ps1
@@ -1,6 +1,1 @@
-
-$cmd = "aws sts assume-role-with-saml --principal-arn arn:aws:iam::488115367344:saml-provider/ADFS --role-arn arn:aws:iam::488115367344:role/ADFS-BI-Prod-DataDeveloper --saml-assertion file://C:/Users/r1525/saml-assert.txt"
-
-Invoke-Expression -Command $cmd | Tee-Object -Variable res 
-
-Write-Host "Result: $res"
+& "venv/Scripts/python.exe" "src/saml2aws.py"
