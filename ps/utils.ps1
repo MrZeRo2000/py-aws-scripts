5a35a1a46e61b28d94b3aa508597a020fdc223af
MrZeRo2000 <MrZeRo2020@outlook.com>
2024-10-30 12:31:42 +0200

    Added AWS cli script for changing table owner

--- ps/utils.ps1
+++ ps/utils.ps1
@@ -1,2 +1,5 @@
 # List S3 file names and sort
 aws s3 ls s3://sds-prod-ingest-external-sftp-files/adyen-sftp-data/root/ --profile prod --region eu-west-1 | ConvertFrom-String | Where-Object P4 -Like "*received*" | Sort-Object -Property P4| Select-Object -Property P4
+
+# update table owner
+aws dynamodb update-item --table-name sds-prod-common-security-TablesMetadata --key '{\"table_name\": {\"S\": \"get_feedback_survey_detailed_survey_ordered_components_grid_items\"}, \"database_name\": {\"S\": \"customer_shop\"}}' --attribute-updates '{\"data_owner\": {\"Value\": {\"S\": \"c8464\"},\"Action\": \"PUT\"}}' --profile prod --region "eu-west-1"
