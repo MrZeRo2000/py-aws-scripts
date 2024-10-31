ed914eb10dce9f68953dd7afe937701b454a7335
MrZeRo2000 <MrZeRo2020@outlook.com>
2024-10-31 11:43:15 +0200

    Added AWS cli script to get athena query SQL by id

--- ps/utils.ps1
+++ ps/utils.ps1
@@ -3,3 +3,6 @@ aws s3 ls s3://sds-prod-ingest-external-sftp-files/adyen-sftp-data/root/ --profi
 
 # update table owner
 aws dynamodb update-item --table-name sds-prod-common-security-TablesMetadata --key '{\"table_name\": {\"S\": \"get_feedback_survey_detailed_survey_ordered_components_grid_items\"}, \"database_name\": {\"S\": \"customer_shop\"}}' --attribute-updates '{\"data_owner\": {\"Value\": {\"S\": \"c8464\"},\"Action\": \"PUT\"}}' --profile prod --region "eu-west-1"
+
+# get athena query by id
+aws athena get-query-execution --query-execution-id 2469525a-3c8a-4858-b7c3-6916965ab1d9 --profile prod --region eu-west-1
