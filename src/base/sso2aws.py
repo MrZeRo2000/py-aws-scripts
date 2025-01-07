f38c3bc9986d7f4176c64a46a8228643b9effd23
MrZeRo2000 <MrZeRo2020@outlook.com>
2025-01-07 14:19:43 +0200

    Added sso2aws.py

---
+++ src/base/sso2aws.py
@@ -0,0 +1,53 @@
+"""
+Script Name: sso2aws
+Author: Roman
+Date: 07.01.2025
+Description: Reads temporary credentials sso.txt file and saves it locally
+Parameters: None
+Input: "sso.txt" file with credentials from SSO web interface
+Output: {user profile}/.aws/credentials
+"""
+
+
+import os
+
+HOME_PATH = os.path.expanduser(os.getenv('USERPROFILE')).replace("\\", "/")
+SSO_FILE_NAME = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../data/sso.txt")).replace("\\", "/")
+CREDENTIALS_FILE_NAME = f"{HOME_PATH}/.aws/credentials"
+
+ENVS = ['dev', 'prod']
+
+def read_sso_file() -> list[str]:
+    with open(SSO_FILE_NAME, 'r', encoding='utf-8') as f:
+        return f.readlines()
+
+def transform_lines(lines: list[str]) -> list[str]:
+    result = []
+
+    for line in lines:
+        env = next((vl for v in line.split('-') if (vl := v.lower()) in ENVS), None)
+        if env is None:
+            result.append(line)
+        else:
+            print(f"Found {env}")
+            result.append(f"[{env}]\n")
+
+    return result
+
+def write_credentials_file(file_name: str, lines: list[str]) -> None:
+    credentials_file_name = file_name
+
+    with open(credentials_file_name, 'w') as f:
+        f.writelines(lines)
+
+    print(f"Credentials written to {credentials_file_name}")
+
+
+if __name__ == '__main__':
+    print("Reading SSO file...")
+    sso_file_lines = read_sso_file()
+    print(f"SSO file: {len(sso_file_lines)} lines read")
+
+    transformed_lines = transform_lines(sso_file_lines)
+
+    write_credentials_file(CREDENTIALS_FILE_NAME, transformed_lines)
