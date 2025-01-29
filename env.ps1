8356360582eb91321a6404c3079cf252e8038f09
MrZeRo2000 <MrZeRo2020@outlook.com>
2025-01-29 11:12:43 +0200

    Added requirements and env for running eralchemy

--- env.ps1
+++ env.ps1
@@ -1,2 +1,2 @@
-$env:Path += ";$env:LOCALAPPDATA\Programs\Python\Python311\"
+$env:Path += ";$env:LOCALAPPDATA\Programs\Python\Python311\;$env:LOCALAPPDATA\Programs\Graphviz-12.1.2-win64\bin"
 echo $env:Path
